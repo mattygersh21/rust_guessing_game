db3b2c80d942c290ed4246a70b48db5d906eae0e
Matthew Gerszewski <dakotaseia@hotmail.com>
2022-11-16 17:54:00 -0600

    * Initialized project.

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Rust\projects\rust_guessing_game\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Rust\projects\rust_guessing_game\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Rust\projects\rust_guessing_game\guessing_game\target\debug\guessing_game.exe: C:\Rust\projects\rust_guessing_game\guessing_game\src\main.rs
